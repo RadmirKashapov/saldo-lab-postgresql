03ea9f654df0479625c60d1940614be41c00bdf3
Radmir Kashapov <radmir.kashapov.150700@gmail.com>
2020-05-27 18:27:12 +0300

    Added IService impl

---
+++ Services/PaymentService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaldoLab.Web.Services
+{
+    public class PaymentService
+    {
+    }
+}
